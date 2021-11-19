5a87dbdd3624cb58dd05fb74885608a0294bb6cf
Aaron Gonzalez <aaronraulgonzalez@gmail.com>
2021-11-19 16:40:39 -0600

    Adding Users

--- seed.py
+++ seed.py
@@ -7,16 +7,17 @@ from model import User, Rating, Movie, connect_to_db, db
 from server import app
 
 
-def load_users(user_filename):
+def load_users():
     """Load users from u.user into database."""
 
     print("Users")
 
-    for i, row in enumerate(open(user_filename)):
+    for i, row in enumerate(open("seed_data/u.user")):
         row = row.rstrip()
         user_id, age, gender, occupation, zipcode = row.split("|")
 
-        user = User(age=age,
+        user = User(user_id=user_id,
+                    age=age,
                     zipcode=zipcode)
 
         # We need to add to the session or it won't ever be stored
@@ -30,12 +31,12 @@ def load_users(user_filename):
     db.session.commit()
 
 
-def load_movies(movie_filename):
+def load_movies():
     """Load movies from u.item into database."""
 
     print("Movies")
 
-    for i, row in enumerate(open(movie_filename)):
+    for i, row in enumerate(open("seed_data/u.item")):
         row = row.rstrip()
 
         # clever -- we can unpack part of the row!
@@ -53,7 +54,8 @@ def load_movies(movie_filename):
 
         title = title[:-7]   # " (YEAR)" == 7
 
-        movie = Movie(title=title,
+        movie = Movie(movie_id=movie_id,
+                      title=title,
                       released_at=released_at,
                       imdb_url=imdb_url)
 
@@ -68,12 +70,12 @@ def load_movies(movie_filename):
     db.session.commit()
 
 
-def load_ratings(rating_filename):
+def load_ratings():
     """Load ratings from u.data into database."""
 
     print("Ratings")
 
-    for i, row in enumerate(open(rating_filename)):
+    for i, row in enumerate(open("seed_data/u.data")):
         row = row.rstrip()
 
         user_id, movie_id, score, timestamp = row.split("\t")
@@ -124,10 +126,72 @@ if __name__ == "__main__":
     connect_to_db(app)
     db.create_all()
 
-    user_filename = "seed_data/u.user"
-    movie_filename = "seed_data/u.item"
-    rating_filename = "seed_data/u.data"
-    load_users(user_filename)
-    load_movies(movie_filename)
-    load_ratings(rating_filename)
+    load_users()
+    load_movies()
+    load_ratings()
     set_val_user_id()
+
+    # Mimic what we did in the interpreter, and add the Eye and some ratings
+    eye = User(email="the-eye@of-judgment.com", password="evil")
+    db.session.add(eye)
+    db.session.commit()
+
+    # Toy Story
+    r = Rating(user_id=eye.user_id, movie_id=1, score=1)
+    db.session.add(r)
+
+    # Robocop 3
+    r = Rating(user_id=eye.user_id, movie_id=1274, score=5)
+    db.session.add(r)
+
+    # Judge Dredd
+    r = Rating(user_id=eye.user_id, movie_id=373, score=5)
+    db.session.add(r)
+
+    # 3 Ninjas
+    r = Rating(user_id=eye.user_id, movie_id=314, score=5)
+    db.session.add(r)
+
+    # Aladdin
+    r = Rating(user_id=eye.user_id, movie_id=95, score=1)
+    db.session.add(r)
+
+    # The Lion King
+    r = Rating(user_id=eye.user_id, movie_id=71, score=1)
+    db.session.add(r)
+
+    db.session.commit()
+    
+    # Add our user
+    jessica = User(email="jessica@gmail.com",
+                   password="love",
+                   age=42,
+                   zipcode="94114")
+    db.session.add(jessica)
+    db.session.commit()
+
+    # Toy Story
+    r = Rating(user_id=jessica.user_id, movie_id=1, score=5)
+    db.session.add(r)
+
+    # Robocop 3
+    r = Rating(user_id=jessica.user_id, movie_id=1274, score=1)
+    db.session.add(r)
+
+    # Judge Dredd
+    r = Rating(user_id=jessica.user_id, movie_id=373, score=1)
+    db.session.add(r)
+
+    # 3 Ninjas
+    r = Rating(user_id=jessica.user_id, movie_id=314, score=1)
+    db.session.add(r)
+
+    # Aladdin
+    r = Rating(user_id=jessica.user_id, movie_id=95, score=5)
+    db.session.add(r)
+
+    # The Lion King
+    r = Rating(user_id=jessica.user_id, movie_id=71, score=5)
+    db.session.add(r)
+
+    db.session.commit()
